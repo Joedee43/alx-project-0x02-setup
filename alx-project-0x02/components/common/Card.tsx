eea8e513fe9f689bfa083bf8291802f63b5d2e6c
joe dee <joedee4311@gmail.com>
2025-06-07 02:43:46 +0300

    update

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,9 +1,9 @@
-import {CardProps} from "@/interfaces/index"
+import {type CardProps } from "@/interfaces/index"
 
 export default function Card(props:CardProps) {
   return (
     <div>
-      <h1>{props.titles}</h1>
+      <h1>{props.title}</h1>
       <p>{props.content}</p>
     </div>
   )
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -3,12 +3,12 @@ export default function home() {
   return (
     <div>
       <h1>The home page</h1>
-      <Card titles="title 1" content="content 1"/>
-      <Card titles="title 2" content="content 2"/>
-      <Card titles="title 3" content="content 3"/>
-      <Card titles="title 4" content="content 4"/>
-      <Card titles="title 5" content="content 5"/>
-      <Card titles="title 6" content="content 6"/>
+      <Card title="title 1" content="content 1"/>
+      <Card title="title 2" content="content 2"/>
+      <Card title="title 3" content="content 3"/>
+      <Card title="title 4" content="content 4"/>
+      <Card title="title 5" content="content 5"/>
+      <Card title="title 6" content="content 6"/>
     </div>
   )
 }
